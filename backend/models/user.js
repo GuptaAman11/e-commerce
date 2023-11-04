197868f6ef84d8a942930fc1187255ce4e0efc7c
Suraj <srjkushwaha112@gmail.com>
2023-11-05 01:33:42 +0530

    email added

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,6 +11,11 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+     email2222: {
+        type: String,
+        required: true,
+        unique: true
+    },
     password: {
         type: String,
         required: true,

3df171af15ff8964bc3b63f0854d7dbe8b5d5e89
unknown <rajtiwari2177@gmail.com>
2023-11-10 11:36:19 +0530

    basic functions done in controllers

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,22 +6,14 @@ const { verifyJWT, seller } = require('../middlewares/auth');
 
 
 router.post('/addProduct', verifyJWT ,seller, addProduct)
-
 router.put('/updateProduct/:productId', verifyJWT ,seller, updateProduct)
-
 router.get('/getAllProducts',verifyJWT, getAllProducts)
 router.get('/getProductByproductId/:productId',verifyJWT, getProductByproductId)
-
 router.delete('/deleteProduct/:productId' ,deleteProduct)
 
 // router.post('/test', verifyJWT,seller,(req,res)=>{
 //     res.status(200).json({ msg: "welcome Selller" });
-
 // })
-
-
 // router.route('/Profile').post(protect,updateUserProfile)
-
-
 
 module.exports = router;

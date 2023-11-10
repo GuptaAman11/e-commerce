84a6f1d080e4b24f777f4eac037d35c784fedcae
kings-of-king <amangupta9579@gmail.com>
2023-11-10 11:22:49 +0530

    error solved

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,24 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema(
+const productSchema = new Schema({
 
-  {
+  typeOfUserId : {
+    type : mongoose.Types.ObjectId,
+    ref : "User"
+  },
+  name: {
+    type: String,
+    required: true,
+  },
 
-    name: { type: String, required: true },
+  description: {
+    type: String,
+    required: true,
+  },
 
-    description: { type: String, required: true },
+  price: {
+    type: Number,
+    required: true,
+  },
 
-    price: { type: Number, required: true },
+  category: {
+    type: String,
+    required: true,
+  },
+});
 
-    category :{type:String,required :true }
-
-
-
-  }
-
-);
-
-
-module.exports = Product = mongoose.model('Product', productSchema);
+module.exports = Product = mongoose.model("Product", productSchema);

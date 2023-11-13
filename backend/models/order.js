b8dc5de203080e245b95f290e21e8f0df78d0c32
kings-of-king <amangupta9579@gmail.com>
2023-11-13 11:35:25 +0530

    done

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,32 +1,22 @@
 const mongoose = require('mongoose')
 
 const orderSchema = new mongoose.Schema({
-productId :{
-    type : String,
-    enum : ["productsId"],
-    required : true
-},
-userId :{
-    type : String,
-    reuired : true
-},
-address : {
-    city : String,
-    village : String,
-    locality : String,
-    pinNo : Number,
-    road : String
-},
-total : {
-    type : Number
-},
-quntitiy : {
-    type : Number
-},
-typeOfUserId : {
-    type : mongoose.Types.ObjectId,
-    ref : "User"
-},
+
+    productDetail : [{
+        pId : {
+            type : mongoose.Types.ObjectId ,
+            ref : "Product"
+        } ,
+        price : {
+            type: Number ,
+
+        }
+    }] ,
+    userId : {
+        type : mongoose.Types.ObjectId ,
+        ref : "User"
+    }
+
 
 
 })

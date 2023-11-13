e37c675f8aa7c9c92e50bd55ad82fada0d73abc6
unknown <rajtiwari2177@gmail.com>
2023-11-13 16:10:33 +0530

    initialized the fronted but not working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="flex-container">
+
+      
+      <div id="flex-item">
+        <div id="product-head">
+          <img src="/image"></img>
+          <h2>TITLE</h2>
+        </div>
+        <div id="product-info">
+          <h2>
+            <span id="dolar-span">$</span>549
+          </h2>
+        </div>
+      </div>
+
+
+      {/* product2 */}
+
+      <div id="flex-item">
+        <div id="product-head">
+          <img src="/image"></img>
+          <h2>TITLE 2</h2>
+        </div>
+
+        <div id="product-info">
+          <h2>
+            <span id="dolar-span">$</span>899
+          </h2>    
+        </div>
+      </div>
     </div>
   );
+  
 }
 
 export default App;

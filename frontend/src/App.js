f156432f65f7972d78bef7c925266c3a58a23183
kings-of-king <amangupta9579@gmail.com>
2023-11-14 23:42:15 +0530

    msg

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import HomepageCards from "./components/Home/HomePageCards";
+import Navbar from "./components/Home/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HomepageCards />
+    </div>
+  );
+}
+
+export default App;

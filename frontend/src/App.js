6377aed1c0ca63d405e857208010a5f44c867b85
Suraj <srjkushwaha112@gmail.com>
2023-11-14 08:01:29 +0530

    navbar and cards done

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import HomepageCards from './components/HomepageCards';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <HomepageCards/>
+   </>
+  );
+}
+
+export default App;
+
+
+ 

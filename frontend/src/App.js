2a74927e08ec445d3dd287945cabf563f32ccbdc
kings-of-king <amangupta9579@gmail.com>
2023-11-16 20:16:50 +0530

    custom hooks done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import HomepageCards from "./components/Home/HomePageCards";
 import Navbar from "./components/Home/Navbar";
+import { BrowserRouter as Router ,Routes ,Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Login from "./components/Login/Login";
+import SignUp from "./components/Login/Signup";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
-      <HomepageCards />
+
+      <Router>
+        <Routes>
+            <Route path="/landingPage" element={<LandingPage />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/signup" element={<SignUp />}/>
+            <Route path="/" element={<HomepageCards />}/>
+
+        </Routes>
+      </Router>
+
+
     </div>
   );
 }
